22efd42f0257c794e2bb3d160355ecfafd47c8c7
Gabriel Giran Vieira Nader <nader@MacBook-Pro-de-Gabriel.local>
2018-11-16 10:10:16 -0200

    Fix layout of 'pesquisar.php'

--- view/pesquisar.php
+++ view/pesquisar.php
@@ -14,180 +14,207 @@ set_include_path("../control/");
 include("navbar.php");
 require("database.php");?>
 
-<form action="pesquisar.php?s=1" method="POST">
-    Principal filtro: <select name="filtro">
-        <option value="nome">Nome</option>
-        <option value="email">Email</option>
-        <option value="tipo">Tipo de diabetes</option>
-        <option value="sexo">Sexo</option>
-        <option value="city">Cidade</option>
-    </select>
-    <input type="text" name="name" placeholder="Digite um nome">
-    <input type="email" name="email" placeholder="Digite o email">
-
-    <select name="d_type">
-        <option value="null">Selecione um tipo</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-    </select>
-
-    <select name="sexo">
-        <option value="null">Selecione um sexo</option>
-        <option value="masc">Masculino</option>
-        <option value="fem">Feminino</option>
-        <option value="outro">Outro</option>
-    </select>
-
-    <select name="city">
-        <option value="null">Selecione uma cidade</option>
-        <?php
-            $getCitys = $db->query("SELECT `name` FROM `city`");
-            $qnt = 0;
-            while($row = $getCitys->fetch()){
-                $qnt++;
-                $cityname = $row["name"];
-               ?>
-               <option value="<?php echo $cityname;?>"><?php echo $cityname;?></option>
-            <?php }
-        ?>
-
-    <input type="submit" value="Procurar">
-
-    <?php 
-        if(!isset($_GET["s"])){
-
-        }else{
-            $query = "SELECT * FROM `user` WHERE";
-            $nome = isset($_POST["name"]) ? $_POST["name"] : "null";
-            $email = isset($_POST["email"]) ? $_POST["email"] : "null";
-            $dtype = isset($_POST["d_type"]) ? $_POST["d_type"] : "null";
-            $sexo = isset($_POST["sexo"]) ? $_POST["sexo"] : "null";
-            $city = isset($_POST["city"]) ? $_POST["city"] : "null";
-            $filtro = $_POST["filtro"];
-
-            $activeNome = false;
-            $activeEmail = false;
-            $activeDType = false;
-            $activeSexo = false;
-            $activeCity = false;
-
-            $cityID = 0;
-            $citySearch = $db->query("SELECT `id` FROM `city` WHERE `name`='".$city."'");
-            if($row = $citySearch->fetch()){
-                $cityID = $row["id"];
-            }
-            if($filtro == "nome"){
-                $query .= " `nome`='".$nome."'";
-                $activeNome = true;
-            }else if($filtro == "email"){
-                $query .= " `email`='".$email."'";
-                $activeEmail = true;
-            }else if($filtro == "tipo"){
-                $query .= " `tipo_diabetes`='".$dtype."'";
-                $activeDType = true;
-            }else if($filtro == "sexo"){
-                $query .= " `sexo`='".$sexo."'";
-                $activeSexo = true;
-            }else if($filtro == "city"){
-                $query .= " `cidade`='".$cityID."'";
-                $activeCity = true;
-            }
-
-            if($nome != ""){
-                $query .= " AND `nome`='".$nome."'";
-                $activeNome = true;
-            }
-            if($email != ""){
-                $query .= " AND `email`='".$email."'";
-                $activeEmail = true;
-            }
-            if($dtype != "null"){
-                $query .= " AND `tipo_diabetes`='".$dtype."'";
-                $activeDType = true;
-            }
-            if($sexo != "null"){
-                $query .= " AND `sexo`='".$sexo."'";
-                $activeSexo = true;
-            }
-            if($city != "null"){
-                $query .= " AND `cidade`='".$cityID."'";
-                $activeCity = true;
-            }
-    ?>
-    <div class="container">
-            <p class="text-center font-weight-bold">Pesquisas por:</p>
-            <?php 
-                if($activeNome){
-                    echo '<p class="text-center font-weight-bold text-success">NOME: VERDADEIRO</p>';
-                }else{
-                    echo '<p class="text-center font-weight-bold text-danger">NOME: FALSO</p>';
-                }
-                if($activeEmail){
-                    echo '<p class="text-center font-weight-bold text-success">EMAIL: VERDADEIRO</p>';
-                }else{
-                    echo '<p class="text-center font-weight-bold text-danger">EMAIL: FALSO</p>';
-                }
-                if($activeDType){
-                    echo '<p class="text-center font-weight-bold text-success">Tipo de diabetes: VERDADEIRO</p>';
-                }else{
-                    echo '<p class="text-center font-weight-bold text-danger">Tipo de diabetes: FALSO</p>';
-                }
-                if($activeSexo){
-                    echo '<p class="text-center font-weight-bold text-success">Sexo: VERDADEIRO</p>';
-                }else{
-                    echo '<p class="text-center font-weight-bold text-danger">Sexo: FALSO</p>';
-                }
-                if($activeCity){
-                    echo '<p class="text-center font-weight-bold text-success">Cidade: VERDADEIRO</p>';
-                }else{
-                    echo '<p class="text-center font-weight-bold text-danger">Cidade: FALSO</p>';
-                }
-                
-            
-            ?>
-            
-    </div>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Nome</th>
-                <th scope="col">E-mail</th>
-                <th scope="col">Endereço</th>
-                <th scope="col">Data de nascimento</th>
-                <th scope="col">Cidade</th>
-                <th scope="col">Acao</th>
-            </tr>
-        </thead>
-        <?php
-            $selectAll = $db->query($query);
-            while($row = $selectAll->fetch()){
-                $id = $row["id"];
-                $nome = $row["nome"];
-                $email = $row["email"];
-                $adress = $row["endereco"];
-                $build = $row["born"];
-                $cityID = $row["cidade"];
-                $getCityName = "SELECT `name` FROM `city` WHERE `id`='".$cityID."'";
-                $getCityNameQuery = $db->query($getCityName);
-                $cityName = "Não encontrado!";
-                if($citys = $getCityNameQuery->fetch()){
-                    $cityName = $citys["name"];
-                }
-                $born = date("d/m/Y", strtotime($build));
-        ?>
-        <tr>
-            <td><?php echo $nome; ?></td>
-            <td><?php echo $email; ?></td>
-            <td><?php echo $adress; ?></td>
-            <td><?php echo $born; ?></td>
-            <td><?php echo $cityName; ?></td>
-            <td><a href="user-profile.php?action=details&id=<?php echo $id; ?>" class="btn btn-success btn-sm">Ver detalhes</a></td>
-        </tr>
-    <?php }
-?> 
+    <div class="container my-3">
     
-    </table>
-</form>
+        <form class="form-row mt-2" action="pesquisar.php?s=1" method="POST">
+
+        <div class="col-12 row mb-2 mx-auto">
+            <label class="col-12 col-md-2 col-form-label" for="filtro">Principal filtro:</label>
+            <select class="form-control col-12 col-md-10" name="filtro" id="filtro">
+                <option value="nome">Nome</option>
+                <option value="email">Email</option>
+                <option value="tipo">Tipo de diabetes</option>
+                <option value="sexo">Sexo</option>
+                <option value="city">Cidade</option>
+            </select>
+        </div>
+        
+
+            <div class="col-12 col-md-6 mb-2">
+                <input class="form-control" type="text" name="name" placeholder="Digite um nome">
+            </div>
+
+            <div class="col-12 col-md-6 mb-2">
+                <input class="form-control" type="email" name="email" placeholder="Digite o email">
+            </div>
+
+            <div class="col-12 col-md-4 mb-2">
+                <select class="form-control" name="d_type">
+                    <option value="null">Selecione um tipo</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                </select>
+            </div>
+
+            <div class="col-12 col-md-4 mb-2">
+                <select class="form-control" name="sexo">
+                    <option value="null">Selecione um sexo</option>
+                    <option value="masc">Masculino</option>
+                    <option value="fem">Feminino</option>
+                    <option value="outro">Outro</option>
+                </select>
+            </div>
+
+            <div class="col-12 col-md-4 mb-2">
+                <select class="form-control" name="city">
+                    <option value="null">Selecione uma cidade</option>
+                    <?php
+                        $getCitys = $db->query("SELECT `name` FROM `city`");
+                        $qnt = 0;
+                        while($row = $getCitys->fetch()){
+                            $qnt++;
+                            $cityname = $row["name"];
+                        ?>
+                        <option value="<?php echo $cityname;?>"><?php echo $cityname;?></option>
+                        <?php }
+                    ?>
+                </select>
+            </div>
+
+
+
+        <input class="btn btn-primary ml-1" type="submit" value="Procurar">
+    
+
+
+        <?php 
+            if(!isset($_GET["s"])){
+
+            }else{
+                $query = "SELECT * FROM `user` WHERE";
+                $nome = isset($_POST["name"]) ? $_POST["name"] : "null";
+                $email = isset($_POST["email"]) ? $_POST["email"] : "null";
+                $dtype = isset($_POST["d_type"]) ? $_POST["d_type"] : "null";
+                $sexo = isset($_POST["sexo"]) ? $_POST["sexo"] : "null";
+                $city = isset($_POST["city"]) ? $_POST["city"] : "null";
+                $filtro = $_POST["filtro"];
+
+                $activeNome = false;
+                $activeEmail = false;
+                $activeDType = false;
+                $activeSexo = false;
+                $activeCity = false;
+
+                $cityID = 0;
+                $citySearch = $db->query("SELECT `id` FROM `city` WHERE `name`='".$city."'");
+                if($row = $citySearch->fetch()){
+                    $cityID = $row["id"];
+                }
+                if($filtro == "nome"){
+                    $query .= " `nome`='".$nome."'";
+                    $activeNome = true;
+                }else if($filtro == "email"){
+                    $query .= " `email`='".$email."'";
+                    $activeEmail = true;
+                }else if($filtro == "tipo"){
+                    $query .= " `tipo_diabetes`='".$dtype."'";
+                    $activeDType = true;
+                }else if($filtro == "sexo"){
+                    $query .= " `sexo`='".$sexo."'";
+                    $activeSexo = true;
+                }else if($filtro == "city"){
+                    $query .= " `cidade`='".$cityID."'";
+                    $activeCity = true;
+                }
+
+                if($nome != ""){
+                    $query .= " AND `nome`='".$nome."'";
+                    $activeNome = true;
+                }
+                if($email != ""){
+                    $query .= " AND `email`='".$email."'";
+                    $activeEmail = true;
+                }
+                if($dtype != "null"){
+                    $query .= " AND `tipo_diabetes`='".$dtype."'";
+                    $activeDType = true;
+                }
+                if($sexo != "null"){
+                    $query .= " AND `sexo`='".$sexo."'";
+                    $activeSexo = true;
+                }
+                if($city != "null"){
+                    $query .= " AND `cidade`='".$cityID."'";
+                    $activeCity = true;
+                }
+        ?>
+        <div class="container">
+                <p class="text-center font-weight-bold">Pesquisas por:</p>
+                <?php 
+                    if($activeNome){
+                        echo '<p class="text-center font-weight-bold text-success">NOME: VERDADEIRO</p>';
+                    }else{
+                        echo '<p class="text-center font-weight-bold text-danger">NOME: FALSO</p>';
+                    }
+                    if($activeEmail){
+                        echo '<p class="text-center font-weight-bold text-success">EMAIL: VERDADEIRO</p>';
+                    }else{
+                        echo '<p class="text-center font-weight-bold text-danger">EMAIL: FALSO</p>';
+                    }
+                    if($activeDType){
+                        echo '<p class="text-center font-weight-bold text-success">Tipo de diabetes: VERDADEIRO</p>';
+                    }else{
+                        echo '<p class="text-center font-weight-bold text-danger">Tipo de diabetes: FALSO</p>';
+                    }
+                    if($activeSexo){
+                        echo '<p class="text-center font-weight-bold text-success">Sexo: VERDADEIRO</p>';
+                    }else{
+                        echo '<p class="text-center font-weight-bold text-danger">Sexo: FALSO</p>';
+                    }
+                    if($activeCity){
+                        echo '<p class="text-center font-weight-bold text-success">Cidade: VERDADEIRO</p>';
+                    }else{
+                        echo '<p class="text-center font-weight-bold text-danger">Cidade: FALSO</p>';
+                    }
+                    
+                
+                ?>
+                
+        </div>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">Nome</th>
+                    <th scope="col">E-mail</th>
+                    <th scope="col">Endereço</th>
+                    <th scope="col">Data de nascimento</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Acao</th>
+                </tr>
+            </thead>
+            <?php
+                $selectAll = $db->query($query);
+                while($row = $selectAll->fetch()){
+                    $id = $row["id"];
+                    $nome = $row["nome"];
+                    $email = $row["email"];
+                    $adress = $row["endereco"];
+                    $build = $row["born"];
+                    $cityID = $row["cidade"];
+                    $getCityName = "SELECT `name` FROM `city` WHERE `id`='".$cityID."'";
+                    $getCityNameQuery = $db->query($getCityName);
+                    $cityName = "Não encontrado!";
+                    if($citys = $getCityNameQuery->fetch()){
+                        $cityName = $citys["name"];
+                    }
+                    $born = date("d/m/Y", strtotime($build));
+            ?>
+            <tr>
+                <td><?php echo $nome; ?></td>
+                <td><?php echo $email; ?></td>
+                <td><?php echo $adress; ?></td>
+                <td><?php echo $born; ?></td>
+                <td><?php echo $cityName; ?></td>
+                <td><a href="user-profile.php?action=details&id=<?php echo $id; ?>" class="btn btn-success btn-sm">Ver detalhes</a></td>
+            </tr>
+        <?php }
+    ?> 
+        
+        </table>
+    </form>
+
+    </div>
+
         <?php }
         ?>
 </body>
